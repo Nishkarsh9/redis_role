643fffc3ff34c25b58adafc9eb2e5eebfaba33d1
Nishkarsh9 <115409219+Nishkarsh9@users.noreply.github.com>
2025-02-04 20:34:31 +0530

    Update redis_tool.groovy

--- vars/redis_tool.groovy
+++ vars/redis_tool.groovy
@@ -5,7 +5,7 @@ def call() {
     // Step 1: Clone the specific Git repository and checkout the desired branch
     sh 'git clone https://github.com/Nishkarsh9/image.git'
 
-     // Step 2: User Approval (if needed)
+    // Step 2: User Approval (if needed)
     if (config.KEEP_APPROVAL_STAGE.toBoolean()) {
         input message: 'Approve to continue with Redis deployment?', ok: 'Deploy'
     }
@@ -17,9 +17,9 @@ def call() {
             extraVars: [
                 CODE_BASE_PATH: config.CODE_BASE_PATH,
                 ENVIRONMENT: config.ENVIRONMENT
-            ]
-          become: true,  // If you need to run with sudo privileges
-          becomeUser: 'root'  // Optionally specify user (if needed)
+            ],
+            become: true,  // If you need to run with sudo privileges
+            becomeUser: 'root'  // Optionally specify user (if needed)
         )
     } catch (Exception e) {
         currentBuild.result = 'FAILURE'
@@ -29,5 +29,3 @@ def call() {
     // Step 4: Send Notification to Slack
     slackSend(channel: config.SLACK_CHANNEL_NAME, message: config.ACTION_MESSAGE)
 }
-}
-
